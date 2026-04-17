a82f08bd2b9706bb5b3ee3dcc714957668262430
Дмитрий Даурбеков <daurbekovdmitrij@gmail.com>
2026-04-17 22:05:20 +0300

    Add tip annotation

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from random import randint
 
 
-def attack(char_name, char_class):
+def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный {5 +
                                                             randint(3, 5)}')
@@ -11,9 +11,11 @@ def attack(char_name, char_class):
     if char_class == 'healer':
         return (f'{char_name} нанёс урон противнику равный {5 +
                                                             randint(-3, -1)}')
+    else:
+        return 'Выберите один из вариантов'
 
 
-def defence(char_name, char_class):
+def defence(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 +
                                           randint(5, 10)} урона')
@@ -24,20 +26,26 @@ def defence(char_name, char_class):
         return (f'{char_name} блокировал {10 +
                                           randint(2, 5)} урона')
 
+    else:
+        return 'Выберите один из вариантов'
 
-def special(char_name, char_class):
+
+def special(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
-    if char_class == 'mage':
+    elif char_class == 'mage':
         return (f'{char_name} применил специальное умение '
                 f'«Атака {5 + 40}»')
-    if char_class == 'healer':
+    elif char_class == 'healer':
         return (f'{char_name} применил специальное умение '
                 f'«Защита {10 + 30}»')
 
+    else:
+        return 'Выберите один из вариантов'
 
-def start_training(char_name, char_class):
+
+def start_training(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
